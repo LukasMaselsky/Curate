607158df603b7047bfd90ebb776505ea12359517
LukasMaselsky <lukasmaselsky@gmail.com>
2024-01-24 14:57:42 +0000

    planetscale

--- server/db.js
+++ server/db.js
@@ -2,9 +2,4 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+export const db = mysql.createConnection(process.env.DATABASE_URL);
