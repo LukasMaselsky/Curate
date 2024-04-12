7ffc7f3019b6274412b3f3db8ece3718d0eef1cb
LukasMaselsky <lukasmaselsky@gmail.com>
2024-04-12 14:27:23 +0100

    temp fix to home page staying hidden on navigating

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,22 @@
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import HomeContent from "../../components/Home/HomeContent";
+import { useEffect } from "react";
 
 export default function Home({ search, setSearch }) {
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                }
+            });
+        });
+
+        const hiddenElements = document.querySelectorAll(".hidden");
+        hiddenElements.forEach((el) => observer.observe(el));
+    });
+
     return (
         <div className="hidden">
             <Navbar search={search} setSearch={setSearch} />
